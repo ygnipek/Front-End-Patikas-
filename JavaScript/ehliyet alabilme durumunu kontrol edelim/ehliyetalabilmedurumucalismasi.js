bd42c84c3168c5799bbc8eef4464007e0349553e
[ygnipek] <[ipekyagan@gmail.com]>
2022-09-29 17:10:42 +0300

    Ehliyet Alabilme Kosulu Calismasi Duzenlendi.

--- JavaScript/ehliyet alabilme durumunu kontrol edelim/ehliyetalabilmedurumucalismasi.js
+++ JavaScript/ehliyet alabilme durumunu kontrol edelim/ehliyetalabilmedurumucalismasi.js
@@ -4,9 +4,9 @@ let age = prompt("Yaşınızı Giriniz:")
 let info = document.querySelector("#info")
 
 if (userName && age >= 18) {
-    info.innerHTML = "Ehliyet Alabilirsiniz."
+    info.innerHTML = ` ${userName},  Ehliyet Alabilirsiniz.`
 } else if (!userName) {
-    info.innerHTML = "Adınızı Girmediğiniz İçin Kullanıcı Bilginiz Eşleştirilemedi."
+    info.innerHTML = `Adınızı Girmediğiniz İçin Kullanıcı Bilginiz Eşleştirilemedi.`
 } else if (!(age>=18)) {
-    info.innerHTML = "Yaş Bilginiz Ehliyet Almak İçin Uygun Aralıkta Değil."
+    info.innerHTML = `${userName}, Yaş Bilginiz Ehliyet Almak İçin Uygun Aralıkta Değil.`
 }
